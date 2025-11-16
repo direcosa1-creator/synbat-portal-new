55f6264305d3e0705505deefdf9a3ba24038dc50
Sinergy <direcosa1@gmail.com>
2025-11-16 22:13:58 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
 
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'system-ui,Segoe UI,Roboto,Arial', padding: 20 }}>
+      <h1>SynBat Portal</h1>
+      <p>Se vedi questa pagina online, Vercel sta funzionando.</p>
+    </main>
+  )
+}
